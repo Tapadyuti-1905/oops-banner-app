30e3a27f64728c89ebf350366e8876e542ebf98c
Tapadyuti Khan <tapadyuti1905@gmail.com>
2026-02-16 01:05:52 +0530

    First commit for OOPS Banner App

---
+++ OOPSBannerAppUC1.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerAppUC1
+ *
+ * UC1: Print the literal text "OOPS" to the console.
+ *
+ * @author Tapadyuti
+ * @version 1.0
+ */
+public class OOPSBannerAppUC1 {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
