d6b899c62fb156e9bfe8acd94de7ae4c4510a54a
Tapadyuti Khan <tapadyuti1905@gmail.com>
2026-02-16 10:36:46 +0530

    UC2: Display OOPS banner using ASCII pattern

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,24 @@
+/**
+ * OOPSBannerAppUC2
+ *
+ * UC2:Render OOPS as Banner using Print Statements
+
+ *
+ * @author Tapadyuti
+ * @version 1.0
+ */
+public class OOPSBannerAppUC2
+{
+public static void main(String args[])
+{
+System.out.println("      ******      "   +   "       ******      "   +   "  ********      "   +   "     ******      " );
+System.out.println("    **      **    "   +   "     **      **    "   +   "  **      **    "   +   "   **            " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **        **  "   +   "  **             " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **      **    "   +   "    **           " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  ** ******     "   +   "      ****       " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **            "   +   "           **    " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **            "   +   "            **   " );
+System.out.println("    **      **    "   +   "     **      **    "   +   "  **            "   +   "          **     " );
+System.out.println("      ******      "   +   "       ******      "   +   "  **            "   +   "    ******       " );
+}
+}
